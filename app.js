d86e62fb072d4d9f66fd2cd75edd3899a4731ae7
bgrim <bgrimshaw37@gmail.com>
2021-09-21 18:47:53 -0600

    hopefully a working project

---
+++ app.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+const express = require('express');
+
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(express.urlencoded({extended:false}));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3000);
