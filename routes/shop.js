246e22dc382a7d618670444486a818de188baa06
bgrim <bgrimshaw37@gmail.com>
2021-09-21 21:20:31 -0600

    update on project with express

--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,15 @@
 const path = require('path');
 
+
 const express = require('express');
 
 const rootDir = require('../util/path');
+const adminData = require('./admin'); //imports admin.js where we have exported routes and the products array
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
+  console.log(adminData.products);
   res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
