d8236f85dc61046609c378acd60f890bbadc4c03
bgrim <bgrimshaw37@gmail.com>
2021-09-28 11:13:20 -0600

    push port for listen

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const path = require('path');
-
+const PORT = process.env.PORT || 5000;
 const express = require('express');
 
 const app = express();
@@ -21,4 +21,4 @@ app.use((req, res, next) => {
     res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 
-app.listen(3000);
+app.listen(PORT);   
